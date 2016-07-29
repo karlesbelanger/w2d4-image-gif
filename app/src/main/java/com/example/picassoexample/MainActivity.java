eca255c219fdbdfc84a859b1a652ac5d49e043aa
karlesbelanger <karlesbelanger@gmail.com>
2016-07-29 15:35:57 -0400

    Glide with gif image done

--- app/src/main/java/com/example/picassoexample/MainActivity.java
+++ app/src/main/java/com/example/picassoexample/MainActivity.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView mImageView;
+    private ImageView mImageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mImageView = (ImageView) findViewById(R.id.a_main_img);
+        mImageView2 = (ImageView) findViewById(R.id.a_main_img2);
     }
 
     public void doMagic(View view) {
-        Picasso.with(this)
-                .load("https://pbs.twimg.com/profile_images/705882875896279042/uHCh8ynL.jpg")
-                .into(mImageView);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(mImageView);
+        Glide.with(this)
+                .load("https://usatsneakhype.files.wordpress.com/2014/04/boob-gifs-vs-butt-gifs-122.gif")//http://likegif.com/wp-content/uploads/2013/03/01/100-funny-girl-gifs-29.gif
+                .into(imageViewTarget);
+        imageViewTarget = new GlideDrawableImageViewTarget(mImageView2);
+        Glide.with(this)
+            .load("http://likegif.com/wp-content/uploads/2013/03/01/100-funny-girl-gifs-29.gif")
+            .into(imageViewTarget);
     }
 }
